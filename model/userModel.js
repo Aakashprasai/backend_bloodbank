0134792da8886734a1c77537ebfb3820a959a326
Aakashprasai <aakashprasai2002@gmail.com>
2023-12-17 13:41:48 +0545

    changes

---
+++ model/userModel.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  number: {
+    type: Number,
+    required: true,
+  },
+  currentAddress: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  confirmPassword: {
+    type: String,
+    required: false,
+  },
+});
+
+const user = mongoose.model("user", userSchema);
+module.exports = user;
